e888de3004b3bc3b546c38bdeb193f55248c54b6
kundai3 <kundaingwereva3@gmail.com>
2026-06-25 11:28:27 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,1 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(){
 
+    const removeButtons = document.querySelectorAll(".remove-btn");
+
+    removeButtons.forEach(button => {
+        button.addEventListener("click", function(){
+            this.parentElement.remove();
+            alert("Item removed from cart");
+        });
+    });
+
+    const minus = document.getElementById("minus");
+    const plus = document.getElementById("plus");
+    const quantity = document.getElementById("quantity");
+
+    if(minus && plus && quantity){
+
+        minus.addEventListener("click", function(){
+            if(quantity.value > 1){
+                quantity.value--;
+            }
+        });
+
+        plus.addEventListener("click", function(){
+            quantity.value++;
+        });
+
+    }
+
+});
