dddb732130235ec12f588f08f10f65da86285d95
kundai3 <kundaingwereva3@gmail.com>
2026-06-25 11:56:58 +0200

    Update register.js

--- register.js
+++ register.js
@@ -1,1 +1,24 @@
+const loginForm = document.getElementById("loginForm");
+const registerForm = document.getElementById("registerForm");
 
+if(loginForm){
+    loginForm.addEventListener("submit", function(e){
+        e.preventDefault();
+        alert("Login successful!");
+    });
+}
+
+if(registerForm){
+    registerForm.addEventListener("submit", function(e){
+        e.preventDefault();
+
+        const passwords = registerForm.querySelectorAll('input[type="password"]');
+
+        if(passwords[0].value !== passwords[1].value){
+            alert("Passwords do not match!");
+            return;
+        }
+
+        alert("Registration successful!");
+    });
+}
